58272b20daa67517fa08515b5f6e1c17b4668c60
Dino <kill_death@163.com>
2018-04-07 14:20:57 +0800

    Demo初步搭建

---
+++ StrategyPatternDemo/StrategyPatternDemo/Controller/DORootVC.m
@@ -0,0 +1,25 @@
+//
+//  DORootVC.m
+//  StrategyPatternDemo
+//
+//  Created by 魏欣宇 on 2018/4/7.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"策略模式";
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
---
+++ StrategyPatternDemo/StrategyPatternDemoTests/StrategyPatternDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  StrategyPatternDemoTests.m
+//  StrategyPatternDemoTests
+//
+//  Created by 魏欣宇 on 2018/4/7.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StrategyPatternDemoTests : XCTestCase
+
+@end
+
+@implementation StrategyPatternDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
